42f72637f7212973de03c6be7beafb13d3888cd8
Yana Sozonova <ynakochneva@mail.ru>
2024-09-17 13:07:30 +0500

    инициация классов Product и Category

--- src/product.py
+++ src/product.py
@@ -1,5 +1,14 @@
 class Product:
+    """Класс для представления Продукта"""
+
     name: str
     description: str
     price: float
     quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса продукты. Задаем значения атрибутам экземпляра."""
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity

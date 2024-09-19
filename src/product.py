6c6bf1da197b47d6595bf69dd377bab2411c0498
Yana Sozonova <ynakochneva@mail.ru>
2024-09-19 13:13:53 +0500

    создала  метод new_product для класса Product и тесты к нему

--- src/product.py
+++ src/product.py
@@ -12,3 +12,21 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, dict_product: dict, list_products: list = None):
+        """Метод, который принимает на вход параметры товара в словаре и возвращает
+         созданный объект класса Product"""
+        if list_products:
+            for product in list_products:
+                if product.name == dict_product['name']:
+                    dict_product['quantity'] = dict_product['quantity'] + product.quantity
+
+                    if product.price > dict_product['price']:
+                        dict_product['price'] = product.price
+
+        name = dict_product['name']
+        description = dict_product['description']
+        price = dict_product['price']
+        quantity = dict_product['quantity']
+        return cls(name, description, price, quantity)
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,6 +1,33 @@
+from src.product import Product
 def test_product(product_1):
     """Тестирование корректности инициализации объектов класса Product"""
     assert product_1.name == "Samsung Galaxy S23 Ultra"
     assert product_1.description == "256GB, Серый цвет, 200MP камера"
     assert product_1.price == 23100
     assert product_1.quantity == 5
+
+
+def test_new_product():
+    """Тестирование добавления нового продукта из переданного словаря"""
+    new_product = Product.new_product({"name": "Samsung S27 Ultra",
+                                       "description": "256GB, Розовый цвет, 400MP камера",
+                                       "price": 480000.0,
+                                       "quantity": 2})
+
+    assert new_product.name == "Samsung S27 Ultra"
+    assert new_product.price == 480000.0
+    assert new_product.description == "256GB, Розовый цвет, 400MP камера"
+    assert new_product.quantity == 2
+
+
+def test_new_product_and_list_products(category_1):
+    """Тестирование корректного добавления нового товара, с учетом проверки такого же товара"""
+    new_product = Product.new_product({"name": "Samsung Galaxy S23",
+                                       "description": "256GB, Розовый цвет, 400MP камера",
+                                       "price": 72000.0,
+                                       "quantity": 3}, category_1.products_in_list)
+
+    assert new_product.name == "Samsung Galaxy S23"
+    assert new_product.price == 180000.0
+    assert new_product.quantity == 8
+
